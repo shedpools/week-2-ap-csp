2f7fc9b620e75fc7b2479732160b9464d1a76df5
shedpools <spode16x@gmail.com>
2025-10-28 19:30:31 +0000

    edits

--- 1_variables_and_datatypes.py
+++ 1_variables_and_datatypes.py
@@ -28,17 +28,17 @@ print("The sum is:", sum_result)
 name = "John"
 print("Name:", name)
 
-# # 2. Numbers: Numeric data (integers or floats)
-# age = 23
-# print("Age:", age)
+ # 2. Numbers: Numeric data (integers or floats)
+ age = 23
+ print("Age:", age)
 
-# # 3. Booleans: True or False values
-# hungry = False
-# print("Hungry:", hungry)
+ # 3. Booleans: True or False values
+ hungry = False
+ print("Hungry:", hungry)
 
 
 # # Example combining them all:
-# print(f"{name} is {age} years old. Hungry? {hungry}")
+ print(f"{name} is {age} years old. Hungry? {hungry}")
 
 
 # # ----------------------------------------
